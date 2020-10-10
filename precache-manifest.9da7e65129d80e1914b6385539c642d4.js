887bb259bc827d37a97ce4f66769e159dfce6926
Allison Barnard <allisonbarnard07@gmail.com>
2020-10-10 17:33:01 -0400

    Updates

---
+++ precache-manifest.9da7e65129d80e1914b6385539c642d4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c771be74bc7f01865310f86b5a4807c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b3c9223833f4af06bac4",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "df5da26507634b4335da",
+    "url": "/static/css/main.c1bb9b3b.chunk.css"
+  },
+  {
+    "revision": "b3c9223833f4af06bac4",
+    "url": "/static/js/2.64f82f05.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.64f82f05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df5da26507634b4335da",
+    "url": "/static/js/main.ba21e698.chunk.js"
+  },
+  {
+    "revision": "35839d639db8e60032a1",
+    "url": "/static/js/runtime-main.f78ad96b.js"
+  }
+]);
